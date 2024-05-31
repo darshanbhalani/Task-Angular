46a193142f81bf6d44c16a8b2627b910b95b79cd
DarshanBhalani-Amnex <165637028+DarshanBhalani-Amnex@users.noreply.github.com>
2024-05-31 17:41:03 +0530

    commited by darshan

--- src/app/components/configurations/configurations.component.ts
+++ src/app/components/configurations/configurations.component.ts
@@ -1,22 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { GridModule } from '@progress/kendo-angular-grid';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModel } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-configurations',
   standalone: true,
-  imports: [GridModule],
+  imports: [GridModule,HttpClientModule],
   templateUrl: './configurations.component.html',
   styleUrl: './configurations.component.css'
 })
 export class ConfigurationsComponent {
-  configurationsList = [
-    {"name":"OverSpeed"},
-    {"name":"AQI"},
-    {"name":"Street Light"},
-    {"name":"Traffic Light"}
-  ];
+  httpClient = inject(HttpClient);
+  configurationsList = [];
+  modalTitle: string = '';
+  id:number=0;
+  thresholdValue: number = 0;
+  intervalTime: number = 0;
+  
 
-  openEditModal(){
-    
+  constructor(private modalService: NgbModal) {}
+
+  ngOnInit(): void {
+   this.fetchData();
+  }
+
+  fetchData(){
+    this.httpClient.get('http://localhost:5226/Configuration/GetAllConfigurations').subscribe((data: any) => {
+      this.configurationsList = data['body'];
+    });
+  }
+
+  openModal(event:Event,content: any, data:any) {
+    console.warn(data);
+    event.preventDefault();
+    this.id=data.configurationId;
+    this.modalTitle = data.configurationName;
+  
+    this.thresholdValue=data.configurationThreshold;
+    this.intervalTime=data.configurationInterval;
+    console.warn(this.thresholdValue);
+    console.warn(this.intervalTime);
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title',centered: true, size: 'md' });
+  }
+
+  saveChanges(thresholdValue:any,intervalTime:any): void {
+    const postData = {
+      configurationId: this.id,
+      configurationName:this.modalTitle,
+      configurationThreshold: Number(thresholdValue),
+      configurationInterval: Number(intervalTime)
+    };
+
+    this.httpClient.post('http://localhost:5226/Configuration/UpdateConfiguration', postData).subscribe((response:any) => {
+      console.log('Response from API:', response);
+      this.fetchData();
+    }, (error:any) => {
+      console.error('Error from API:', error);
+    });
   }
 }
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,44 +1,55 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input, Output } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CircularCounterComponent } from '../circular-counter/circular-counter.component';
 import { GridModule } from '@progress/kendo-angular-grid';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { EventEmitter } from 'ws';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HttpClientModule,CircularCounterComponent,GridModule],
+  imports: [HttpClientModule, CircularCounterComponent, GridModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
 
-export class DashboardComponent {
-  httpClient=inject(HttpClient);
-  flag1=false;
-  flag2=false;
-  incidentAQI:any=[];
-  incidentOverSpeed:any=[];
+export class DashboardComponent{
+  httpClient = inject(HttpClient);
+  flag1 = false;
+  flag2 = false;
+  incidentAQI: any[] = [];
+  incidentOverSpeed: any[] = [];
   modalTitle: string = '';
+  @Input() checked: boolean = true;
 
-  constructor(private modalService: NgbModal) {}
 
-  ngOnInit():void{
-    this.httpClient.get('https://localhost:7091/AQI/GetAllIncidents').subscribe((data:any) => {
-      this.incidentAQI=data['body'];
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    this.httpClient.get('http://localhost:5226/AQI/GetAllIncidents').subscribe((data: any) => {
+      this.incidentAQI = data['body'];
     });
-    this.httpClient.get('https://localhost:7091/OverSpeed/GetAllIncidents').subscribe((data:any) => {
-      this.incidentOverSpeed=data['body'];
+    this.httpClient.get('http://localhost:5226/OverSpeed/GetAllIncidents').subscribe((data: any) => {
+      this.incidentOverSpeed = data['body'];
     });
   }
 
-  fatchData(message:string){
-    if(message==='AQI'){
-    this.flag1=false;
-    this.flag2=true;
-    }
-    else if(message==='OverSpeed'){
-      this.flag2=false;
-      this.flag1=true;
+  fatchData(message: string): void {
+    if (message === 'AQI') {
+      this.flag1 = false;
+      this.flag2 = true;
+    } else if (message === 'OverSpeed') {
+      this.flag2 = false;
+      this.flag1 = true;
     }
   }
+
+  public pageableSettings: any = {
+    buttonCount: 5,
+    info: true,
+    type: 'numeric',
+    pageSizes: [10, 20, 40, 50, 100, 'All'],
+    previousNext: true
+  };
 }
